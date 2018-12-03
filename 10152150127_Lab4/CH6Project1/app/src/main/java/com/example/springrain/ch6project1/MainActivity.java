2c53390804f17b219fcf00968a5ec17df4814b11
yiwangchunyu <yiwangchunyu@163.com>
2018-12-03 14:29:16 +0800

    调整目录

--- 10152150127_Lab4/CH6Project1/app/src/main/java/com/example/springrain/ch6project1/MainActivity.java
+++ 10152150127_Lab4/CH6Project1/app/src/main/java/com/example/springrain/ch6project1/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.Menu;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 
