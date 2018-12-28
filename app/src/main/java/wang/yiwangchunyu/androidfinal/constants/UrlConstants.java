d9dd97ed45321dd289ba2782ab7aad1aed83b932
yiwangchunyu <yiwangchunyu@163.com>
2018-12-28 17:26:40 +0800

    online

--- app/src/main/java/wang/yiwangchunyu/androidfinal/constants/UrlConstants.java
+++ app/src/main/java/wang/yiwangchunyu/androidfinal/constants/UrlConstants.java
@@ -1,8 +1,8 @@
 package wang.yiwangchunyu.androidfinal.constants;
 
 public class UrlConstants {
-//    public static String DOMAIN="119.29.66.67";
-    public static String DOMAIN="305.yiwangchunyu.wang";
+    public static String DOMAIN="119.29.66.67";
+//    public static String DOMAIN="305.yiwangchunyu.wang";
     public static int PORT=8000;
     public static String URL_PRE="http://"+UrlConstants.DOMAIN+":"+UrlConstants.PORT+"/server";
     public static String URL_MEDIA_PRE="http://"+UrlConstants.DOMAIN+":"+UrlConstants.PORT+"/media/";
